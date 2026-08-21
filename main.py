f1dc5a3f19051aa00031f62c6e6453c003a372c1
Ghost-SY1 <ghost-sy1@users.noreply.github.com>
2026-08-21 14:56:06 +0000

    Re-engineer engine to v3.0-PRO (Zero-Guessing)

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-#!/usr/bin/env python3
-from pathlib import Path
-import sys
-
-sys.path.insert(0, str(Path(__file__).parent / "src"))
-
-from ghost_firmware_analyzer.cli import run
-
-
-if __name__ == "__main__":
+from tools.ghost_extension import run
+if __name__ == '__main__':
     raise SystemExit(run())
